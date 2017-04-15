4ea7823a775410acbf9c4cd8423196468bca2d38
Sjoerd van der Meer <sjoerd.meer@outlook.com>
2017-04-15 03:30:36 +0200

    Add MVC with ping controller

---
+++ src/DeSjoerd.Timewriter.Web/ApiControllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeSjoerd.Timewriter.Web.ApiControllers
+{
+    [Route("/api/[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(DateTime))]
+        public IActionResult Ping()
+        {
+            return Ok(DateTime.UtcNow);
+        }
+    }
+}
--- src/DeSjoerd.Timewriter.Web/Startup.cs
+++ src/DeSjoerd.Timewriter.Web/Startup.cs
@@ -16,6 +16,9 @@ namespace DeSjoerd.Timewriter.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore()
+                .AddApiExplorer()
+                .AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +31,7 @@ namespace DeSjoerd.Timewriter.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
