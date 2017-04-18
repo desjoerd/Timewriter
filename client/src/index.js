d971bee489ab77017dd671375adc2af09f671e34
Sjoerd van der Meer <sjoerd.meer@outlook.com>
2017-04-18 20:50:30 +0200

    Add react-router to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router } from 'react-router-dom';
+
+import Root from './Root';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Root Router={Router} />,
   document.getElementById('root')
 );
